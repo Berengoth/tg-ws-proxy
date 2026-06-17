87be7569465aebe8aea04006bb26b724edd652aa
larl <122608561+Berengoth@users.noreply.github.com>
2026-06-17 09:42:03 +0300

    Update tray_common.py
    Локальный путь для портативного режима

--- utils/tray_common.py
+++ utils/tray_common.py
@@ -38,7 +38,7 @@ def _app_dir() -> Path:
 
 APP_DIR = _app_dir()
 CONFIG_FILE = APP_DIR / "config.json"
-LOG_FILE = APP_DIR / "proxy.log""
+LOG_FILE = APP_DIR / "proxy.log"
 FIRST_RUN_MARKER = APP_DIR / ".first_run_done_mtproto"
 IPV6_WARN_MARKER = APP_DIR / ".ipv6_warned"
 
